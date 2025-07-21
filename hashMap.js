b2be492ab5cb6863fdb46f5c0cece4076db03213
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-07-21 19:16:42 -0300

    create hash method

--- hashMap.js
+++ hashMap.js
@@ -3,4 +3,15 @@ class HashMap {
     this.capacity = 16;
     this.loadFactor = 0.8;
   }
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % 16;
+    }
+
+    return hashCode;
+  }
 }
