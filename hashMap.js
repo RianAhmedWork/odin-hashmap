6586c8233a945cbaf6f8f67eff4868ff7afb0dcd
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-07-21 21:02:21 -0300

    start work on set method, make bucket array null

--- hashMap.js
+++ hashMap.js
@@ -2,6 +2,7 @@ class HashMap {
   constructor() {
     this.capacity = 16;
     this.loadFactor = 0.8;
+    this.buckets = new Array(this.capacity).fill(null);
   }
 
   hash(key) {
@@ -9,9 +10,19 @@ class HashMap {
 
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
-      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % 16;
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
     }
 
     return hashCode;
+  }
+
+  set(key, value) {
+    hashedKey = this.hash(key);
+    const pair = { key: key, value: value };
+    if (this.buckets[hashedKey].key === key) {
+      this.buckets[hashedKey].value = value;
+    } else {
+      this.buckets[hashedKey] = pair;
+    }
   }
 }
